1a255c2c283ca178e62c1601df1536e14351b9b2
dbellettini <dbellettini@ee427ae8-e902-0410-961c-c3ed070cd9f9>
2010-08-06 08:28:10 -0500

    No more subscriber model, using sfGuardUser instead
    git-svn-id: http://svn.symfony-project.com/plugins/dvbNewsletterPlugin/trunk@30567 ee427ae8-e902-0410-961c-c3ed070cd9f9

---
+++ modules/dvbNewsletterSubscription/actions/actions.class.php
@@ -0,0 +1,15 @@
+<?php
+
+require_once dirname(__FILE__).'/../lib/BasedvbNewsletterSubscriptionActions.class.php';
+
+/**
+ * dvbNewsletterSubscription actions.
+ * 
+ * @package    dvbNewsletterPlugin
+ * @subpackage dvbNewsletterSubscription
+ * @author     Davide Bellettini <dbellettini@users.sourceforge.net>
+ * @version    SVN: $Id: actions.class.php 12534 2008-11-01 13:38:27Z Kris.Wallsmith $
+ */
+class dvbNewsletterSubscriptionActions extends BasedvbNewsletterSubscriptionActions
+{
+}
---
+++ modules/dvbNewsletterSubscription/lib/BasedvbNewsletterSubscriptionActions.class.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Base actions for the dvbNewsletterPlugin dvbNewsletterSubscription module.
+ * 
+ * @package     dvbNewsletterPlugin
+ * @subpackage  dvbNewsletterSubscription
+ * @author      Davide Bellettini <dbellettini@users.sourceforge.net>
+ * @version     SVN: $Id: BaseActions.class.php 12534 2008-11-01 13:38:27Z Kris.Wallsmith $
+ */
+abstract class BasedvbNewsletterSubscriptionActions extends sfActions
+{
+}
